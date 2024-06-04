9d7bcba1d1f2e7deb8146cee3f48ba2a797faf6a
diogomoreira09 <diogomoreira.35.16@gmail.com>
2024-06-04 18:51:21 +0100

    auth finish

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +12,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Gate::define('admin', function (User $user) { 
+            // Only "administrator" users can "admin" 
+            return $user->admin; 
+        }); 
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -16,18 +16,19 @@ use App\Http\Controllers\ScreeningsController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* ----- PUBLIC ROUTES ----- */ 
+Route::view('/', 'home')->name('home'); 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
+/* ----- Non-Verified users ----- */ 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+/* ----- Verified users ----- */ 
+Route::middleware('auth', 'verified')->group(function () { 
+    Route::view('/dashboard', 'dashboard')->name('dashboard'); 
 });
 
 require __DIR__.'/auth.php';
